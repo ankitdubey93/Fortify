65fcf104d3aedb1aa1a6fc0646406a2da8056145
Ankit Dubey <ankit19933011@gmail.com>
2025-04-25 16:27:39 +0530

    name display on sign in on dashboard

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -1,15 +1,29 @@
 import express from "express";
 import { authenticateToken, AuthRequest } from "../middleware/authMiddleware";
+import { User } from "../models/User";
 
 const dashboardRouter = express.Router();
 
 dashboardRouter.use(authenticateToken);
 
-dashboardRouter.get("/", (req: AuthRequest, res: express.Response) => {
+dashboardRouter.get("/", async (req: AuthRequest, res: express.Response) => {
   if (!req.user) {
     throw new Error("Not authorized.");
-  } else {
-    res.status(200).json({ message: `Welcome user ${req.user.userId}` });
+  }
+
+  try {
+    const loggedInUser = await User.findOne({ _id: req.user.userId });
+
+    if (!loggedInUser) {
+      res
+        .status(404)
+        .json({ message: "User session expired. Please sign in." });
+      return;
+    }
+
+    res.status(200).json({ message: `Welcome ${loggedInUser.name}` });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error." });
   }
 });
 
