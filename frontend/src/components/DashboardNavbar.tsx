6c15aabc2ecbddf0c4cf26a53b691cf43dd067e2
Ankit Dubey <ankit19933011@gmail.com>
2025-05-17 21:07:37 +0530

    check after yml update

--- frontend/src/components/DashboardNavbar.tsx
+++ frontend/src/components/DashboardNavbar.tsx
@@ -18,7 +18,7 @@ const DashboardNavbar: React.FC<DashboardNavbarProps> = ({
         </div>
         <div>
           <h1 className="text-xl font-bold text-sky-600">
-            Welcome test, hello ankit snow {name}
+            Welcome test, hello final {name}
           </h1>
         </div>
       </div>
