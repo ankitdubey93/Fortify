eaf475966effbaa5607c4121dd193b3057da66c8
Ankit Dubey <ankit19933011@gmail.com>
2025-06-18 11:24:58 +0530

    new frontend setup done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-sky-100">
+      <h1 className="text-3xl font-bold text-sky-800">Fortify</h1>
+    </div>
+  );
+};
+
+export default App;
