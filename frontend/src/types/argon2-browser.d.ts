5cff97d770ccdc32a8c5c5c4f3ec6f04e41abcc2
Ankit Dubey <ankit19933011@gmail.com>
2025-06-21 20:24:26 +0530

    added type declaration for argon2

---
+++ frontend/src/types/argon2-browser.d.ts
@@ -0,0 +1,7 @@
+// src/types/argon2-browser.d.ts
+declare module "argon2-browser/dist/argon2-bundled.min.js" {
+  // You can define specific types here if you know them,
+  // but for quick resolution, an 'any' type works.
+  const argon2: any; // Or define specific functions/objects it exports
+  export default argon2;
+}
